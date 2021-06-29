34919770828c1c100b83ddb170a4e6b8d69da233
i. dos santos <iosorio.santos@gmail.com>
2021-06-29 08:44:39 -0300

    removed associations from factories as associations are made at model definition time

--- __tests__/factories.js
+++ __tests__/factories.js
@@ -3,44 +3,20 @@ const { factory } = require("factory-girl");
 const { Farmer, Document, Address } = require("../src/app/models");
 const CPF = require("cpf");
 
-factory.define(
-  "Farmer",
-  Farmer,
-  {
-    name: faker.name.findName(),
-  },
-  {
-    include: [
-      Farmer.hasOne(Document, { as: "document", foreignKey: "farmerId" }),
-      Farmer.hasOne(Address, { as: "address", foreignKey: "farmerId" }),
-    ],
-  }
-);
+factory.define("Farmer", Farmer, {
+  name: faker.name.findName(),
+});
 
-factory.define(
-  "Document",
-  Document,
-  {
-    type: "CPF",
-    number: CPF.generate(),
-  },
-  {
-    include: [Document.belongsTo(Farmer, { as: "farmer", foreignKey: "id" })],
-  }
-);
+factory.define("Document", Document, {
+  type: "CPF",
+  number: CPF.generate(),
+});
 
-factory.define(
-  "Address",
-  Address,
-  {
-    street: faker.address.streetName(),
-    country: faker.address.country(),
-    state: faker.address.state(),
-    address: faker.address.streetAddress(),
-  },
-  {
-    include: [Address.belongsTo(Farmer, { as: "farmer", foreignKey: "id" })],
-  }
-);
+factory.define("Address", Address, {
+  street: faker.address.streetName(),
+  country: faker.address.country(),
+  state: faker.address.state(),
+  address: faker.address.streetAddress(),
+});
 
 module.exports = factory;
